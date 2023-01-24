2593356f703845d9932c8bf90296e050da9a4a34
chrystiano <63607390+Tatsu-ya@users.noreply.github.com>
2023-01-24 05:27:55 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,99 @@
+data = [
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Apartamento xxx",
+
+                    "IconLeft": "office-building-outline",
+
+
+                    "IconRight": "dots-vertical",
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Casa xxx",
+
+                    "IconLeft": "home-outline",
+
+                    "IconRight": "dots-vertical",
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Loja xxx",
+
+                    "IconLeft": "storefront-outline",
+
+                    "IconRight": "dots-vertical",
+
+
+    },
+
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Casa xxx",
+
+                    "IconLeft": "home-outline",
+                    "IconRight": "dots-vertical",
+
+
+
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Apartamento xxx",
+
+                    "IconLeft": "office-building-outline",
+                    "IconRight": "dots-vertical",
+
+
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Trabalho xxx",
+
+                    "IconLeft": "briefcase-outline",
+                    "IconRight": "dots-vertical",
+
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Fabrica xxx",
+
+                    "IconLeft": "factory",
+                    "IconRight": "dots-vertical",
+
+
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Apartamento xxx",
+
+                    "IconLeft": "office-building-outline",
+                    "IconRight": "dots-vertical",
+
+
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Casa xxx",
+
+                    "IconLeft": "home-outline",
+                    "IconRight": "dots-vertical",
+    },
+    {
+                    "text": "XXXXXXXXX",
+                    "secondary_text": "Rua xxx , 000 - Bairro xxx, cidade xxx",
+                    "tertiary_text": "Loja xxx",
+
+                    "IconLeft": "storefront-outline",
+                    "IconRight": "dots-vertical",
+    },
+]
